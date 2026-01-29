836ed4576815b38509db6e8c39b32fc102c8f2af
Umarmk1444 <umarmk1444@gmail.com>
2026-01-29 22:03:05 +0300

    Update server port and improve logging

--- server.js
+++ server.js
@@ -165,7 +165,8 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = 3001;
-server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
